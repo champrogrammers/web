2a5b6d2cdf45e9da90f3ac3319231b1db36b9950
champrogrammers <Champrogrammers@gmail.com>
2020-07-24 23:59:33 -0500

    Updates

---
+++ precache-manifest.f51f82e788bb33e5f083a062f53ea44e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a66bb3d58a613d1bcad7ebe4fd4fb9b",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "a29dbd4b6988788acbe0",
+    "url": "/web/static/css/main.c27ee03e.chunk.css"
+  },
+  {
+    "revision": "d45627b23452c064dd62",
+    "url": "/web/static/js/2.1a9b3eda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web/static/js/2.1a9b3eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29dbd4b6988788acbe0",
+    "url": "/web/static/js/main.88af0719.chunk.js"
+  },
+  {
+    "revision": "b0d45ee8e162648aeee7",
+    "url": "/web/static/js/runtime-main.48df7c7e.js"
+  },
+  {
+    "revision": "2268323f680a2e4a39afc8c76ef240ac",
+    "url": "/web/static/media/android.2268323f.svg"
+  },
+  {
+    "revision": "b6480230105a05ba19db1306c3fa51c4",
+    "url": "/web/static/media/cham.b6480230.png"
+  },
+  {
+    "revision": "11a9078a0ab6e0595856cbeaab94eeaf",
+    "url": "/web/static/media/cs.11a9078a.svg"
+  },
+  {
+    "revision": "f2bd0c0cb064f241f93ab02be9e524b0",
+    "url": "/web/static/media/javascript.f2bd0c0c.svg"
+  },
+  {
+    "revision": "35091c33ed0ff4b73eff9cef938d09cd",
+    "url": "/web/static/media/sql.35091c33.svg"
+  },
+  {
+    "revision": "7b62ef0914452f02ad45f9b88a91863e",
+    "url": "/web/static/media/ux.7b62ef09.png"
+  }
+]);
