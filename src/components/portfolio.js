633c27f45d6b98e75c7f86e62448a9279b7c97c2
deadchri5 <deadchri5h@gmail.com>
2020-10-01 14:19:56 -0500

    Componente portafolio completo y dinamico

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import "../styles/portfolio.css"
 
+//Importar elemento de portafolio
+import PortfolioElement from './portfolioElement';
+
 class Portfolio extends Component {
 
     render() {
         window.scroll(0, 0);
+
         return (
             <React.Fragment>
                 <div className="tittle-portfolio">
@@ -13,71 +17,9 @@ class Portfolio extends Component {
                         En esta sección usted podrá encontrar los trabajos realizados por nosotros.</p>
                 </div>
                 <div id="portfolio-center">
-                    <div className="itm">
-                        <div className="work-photo">
-                            <img src="https://source.unsplash.com/random" />
-                        </div>
-                        <div className="work-text">
-                            <h3>Interfaz para restaurante</h3>
-                            <p>
-                                Aplicación de ecritorio con el objetivo de hacer consultas a una base de datos alojada en un servidor de
-                                SQLserver, la aplicación facilita la administración de facturas a un restaurante de mariscos de Guadalajara.
-                            </p>
-                            <div id="tags">
-                                <span>
-                                    #Java
-                                </span>
-                                <span>
-                                    #FXML
-                                </span>
-                                <span>
-                                    #SQL
-                                </span>
-                            </div>
-                            <div className="work-buttons">
-                                <button>
-                                    Ver Detallles
-                            </button>
-                            </div>
-                        </div>
+                    <div>
+                        <PortfolioElement />
                     </div>
-
-                    <div id="clear-fix"></div>
-
-                    <div className="itm">
-                        <div className="work-photo">
-                            <img src="https://source.unsplash.com/random" />
-                        </div>
-                        <div className="work-text">
-                            <h3>Página de portafolio de CHAM programmers</h3>
-                            <p>
-                                Es una página web reactiva (SPA) tiene como objetivo mostrar el portafolio de la empresa, sus servicios y
-                                una sección para que los clientes se puedan poner en contacto.
-                                Es la misma página web que usted está viendo en este momento.
-                            </p>
-                            <div id="tags">
-                                <span>
-                                    #ReactJS
-                                </span>
-                                <span>
-                                    #JavaScript
-                                </span>
-                                <span>
-                                    #PHP
-                                </span>
-                                <span>
-                                    #CSS3
-                                </span>
-                                <span>
-                                    #HTML5
-                                </span>
-                            </div>
-                            <div className="work-buttons">
-                                <button> Ver Detallles </button>
-                            </div>
-                        </div>
-                    </div>
-
                     <div className="coming-soon">
                         <span>
                             Próximamente más desarrollos...
---
+++ src/components/portfolioElement.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import "../styles/portfolio.css";
+
+//Texts
+import texts from '../texts/portfolio.json';
+
+class portfolioElment extends Component {
+
+
+    render() {
+
+        function getTags(tags) {
+            const arrTags = [];
+            tags.forEach((tag, index) => {
+                arrTags.push(
+                    <span key={index}>{tag}</span>
+                );
+            });
+            return (
+                arrTags
+            )
+        }
+
+        const items = [];
+
+        texts.works.forEach((work, index) => {
+            items.push(
+                <div key={index} className="itm">
+                    <div className="work-photo">
+                        <img src={work.image} alt={work.title} />
+                    </div>
+                    <div className="work-text">
+                        <h3> {work.title} </h3>
+                        <p> {work.info} </p>
+                        <div id="tags">
+                            {getTags(work.tags)}
+                        </div>
+                        <div className="work-buttons">
+                            <button onClick={() => { window.location.href = work.link }}>
+                                Ver Detallles
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+
+        return (
+            items
+        )
+    }
+}
+
+export default portfolioElment;
